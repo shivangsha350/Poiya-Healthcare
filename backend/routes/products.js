4333796597afe580273e1d2771d8df07ac6fbffb
shivang sharma <shivangsha350@gmail.com>
2026-07-06 13:38:23 +0530

    Updated production API

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -46,7 +46,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   fileFilter: fileFilter,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB limit
+  limits: { fileSize: 100 * 1024 * 1024 }, // 100MB limit
 });
 
 const uploadFields = upload.fields([
